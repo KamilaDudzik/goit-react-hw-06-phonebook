bce95cc67eefe1103e7ab663e65f534a2fc91be2
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-10-07 22:21:54 +0200

    Refactor app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,80 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { nanoid } from "nanoid";
-import Notiflix from "notiflix";
+// import { useState, useEffect } from "react";
+// import { nanoid } from "nanoid";
+// import Notiflix from "notiflix";
 
 import { ContactForm } from "./ContactForm/ContactForm";
 import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./Filter/Filter";
 
 export const App = () => {
-  
-  const [contacts, setContact] = useState([])
-  const [filter, setFilter] = useState("")
-
-  // useEffect(() => {
-  //   const savedContacts = JSON.parse(localStorage.getItem("Contacts_Local_Storage") || "[]")
-  //   setContact(savedContacts)
-  // }, [])
-  
-  useEffect(() => {
-    const savedContacts = JSON.parse(localStorage.getItem("Contacts_Local_Storage"))
-
-    if (savedContacts) {
-      setContact(savedContacts)
-    }
-  }, [])
-
-  useEffect(() => {
-    if (contacts.length > 0) {
-      localStorage.setItem("Contacts_Local_Storage", JSON.stringify(contacts))
-    } else {
-      localStorage.removeItem("Contacts_Local_Storage")
-    }
-  }, [contacts])
-  
-  const newContact = (name, number) => {
-    const contactNames = contacts.map(contact => contact.name)
-
-    if (contactNames.includes(name)) {
-      Notiflix.Notify.info(`${name} is already in contacts`)
-      return
-    }
-    setContact([...contacts, { id: nanoid(), name, number }])
-    Notiflix.Notify.success(`${name} added to contacts`)
-  }
-
-  const filterContacts = () => {
-    return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
-  }
-
-  const deleteContact = (id) => {
-    setContact(contacts.filter(contact => contact.id !== id))
-  }
-
-  const filterChange = value => setFilter(value)
 
     return (
       <div>
         <h1>Phonebook</h1>
-        <ContactForm onSubmit={newContact} />
+        <ContactForm />
         <h2>Contacts</h2>
-        <Filter onChange={filterChange} />
-        <ContactList contacts={filterContacts()} onClick={deleteContact} />
+        <Filter />
+        <ContactList />
       </div>
     )
 }
+
+// base code from hw-04
+// export const App = () => {
+  
+//   const [contacts, setContact] = useState([])
+//   const [filter, setFilter] = useState("")
+
+//   // useEffect(() => {
+//   //   const savedContacts = JSON.parse(localStorage.getItem("Contacts_Local_Storage") || "[]")
+//   //   setContact(savedContacts)
+//   // }, [])
+  
+//   useEffect(() => {
+//     const savedContacts = JSON.parse(localStorage.getItem("Contacts_Local_Storage"))
+
+//     if (savedContacts) {
+//       setContact(savedContacts)
+//     }
+//   }, [])
+
+//   useEffect(() => {
+//     if (contacts.length > 0) {
+//       localStorage.setItem("Contacts_Local_Storage", JSON.stringify(contacts))
+//     } else {
+//       localStorage.removeItem("Contacts_Local_Storage")
+//     }
+//   }, [contacts])
+  
+//   const newContact = (name, number) => {
+//     const contactNames = contacts.map(contact => contact.name)
+
+//     if (contactNames.includes(name)) {
+//       Notiflix.Notify.info(`${name} is already in contacts`)
+//       return
+//     }
+//     setContact([...contacts, { id: nanoid(), name, number }])
+//     Notiflix.Notify.success(`${name} added to contacts`)
+//   }
+
+//   const filterContacts = () => {
+//     return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
+//   }
+
+//   const deleteContact = (id) => {
+//     setContact(contacts.filter(contact => contact.id !== id))
+//   }
+
+//   const filterChange = value => setFilter(value)
+
+//     return (
+//       <div>
+//         <h1>Phonebook</h1>
+//         <ContactForm onSubmit={newContact} />
+//         <h2>Contacts</h2>
+//         <Filter onChange={filterChange} />
+//         <ContactList contacts={filterContacts()} onClick={deleteContact} />
+//       </div>
+//     )
+// }
